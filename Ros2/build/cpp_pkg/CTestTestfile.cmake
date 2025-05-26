baaeb66baea2c580ddd468dfa685ed179861d9ed
sierra-95 <michaelmachohi@gmail.com>
2025-05-26 14:41:02 +0300

    Rename

--- Ros2/build/cpp_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/michael/Documents/AGV/ros-learning/Ros2/src/cpp_pkg
-# Build directory: /home/michael/Documents/AGV/ros-learning/Ros2/build/cpp_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
